81ec1a5a1c1e84fcc5daf92a015f736ad1eac00c
Patotski-AS <7906241@gmail.com>
2021-02-06 11:02:00 +0300

    изменил мейн

--- src/com/quiz/Main.java
+++ src/com/quiz/Main.java
@@ -1,12 +1,10 @@
 package com.quiz;
 
 import com.frame.MainMenuFrame;
-import com.mode.SurvivalGame;
 
 public class Main {
 
     public static void main(String[] args) {
-        SurvivalGame survivalGame= new SurvivalGame();
-//        MainMenuFrame mainMenuFrame = new MainMenuFrame();
+        MainMenuFrame mainMenuFrame = new MainMenuFrame();
     }
 }
