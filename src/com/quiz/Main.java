f5f114fcbea72d3b7aa030c83094e18701cb0fee
Patotski-AS <7906241@gmail.com>
2021-02-06 10:49:12 +0300

    Поменял структуру папок. Вынес файл README.md в папку проекта

--- src/com/quiz/Main.java
+++ src/com/quiz/Main.java
@@ -1,11 +1,12 @@
 package com.quiz;
 
 import com.frame.MainMenuFrame;
+import com.mode.SurvivalGame;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        MainMenuFrame mainMenuFrame = new MainMenuFrame();
+        SurvivalGame survivalGame= new SurvivalGame();
+//        MainMenuFrame mainMenuFrame = new MainMenuFrame();
     }
 }
