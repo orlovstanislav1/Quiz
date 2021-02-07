011cd095069eb0b08370907ef75907f3d6d68756
PhilB93 <77934493+PhilB93@users.noreply.github.com>
2021-02-07 18:44:22 +0300

    отредактировал шрифты и положение кнопок

--- src/com/frame/GameFrame.java
+++ src/com/frame/GameFrame.java
@@ -3,54 +3,50 @@ package com.frame;
 
 // ---------------------------- Quiz ----------------------------
 
-/**
- * GameFrame
- * убрать из класса все лишнее
- * перемеслить не стандартную логику игры в классы-наследники
- */
-//
+import com.mode.SurvivalGame;
 import com.quiz.Question;
 import com.quiz.Scores;
 import com.quiz.Settings;
 
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 import java.util.List;
 
-public class GameFrame implements ActionListener {
-    private final Settings settings = Settings.readSettings();
-    private final Question[] questions = randomQuestions();
+public abstract class GameFrame implements ActionListener {
+    protected final Settings settings = Settings.readSettings();
+    protected final Question[] questions = randomQuestions();
 
-
-    private String answer;
-    private int index;
-    private int correct_guesses = 0;
+    protected String answer;
+    protected int index;
+    protected int correct_guesses = 0;
+    protected int seconds = settings.getSeconds();
     protected int numberLives = settings.getNumberLives();
-    private int seconds = settings.getSeconds();
 
+    protected final JFrame frame = new JFrame();
+    protected final JTextField textField = new JTextField();
+    protected final JTextPane textarea = new JTextPane();
+    protected final JButton buttonA = new JButton();
+    protected final JButton buttonB = new JButton();
+    protected final JButton buttonC = new JButton();
+    protected final JButton buttonD = new JButton();
+    protected final JButton buttonRepeat = new JButton();
+    protected final JButton buttonToMenu = new JButton();
+    protected final JLabel answer_labelA = new JLabel();
+    protected final JLabel answer_labelB = new JLabel();
+    protected final JLabel answer_labelC = new JLabel();
+    protected final JLabel answer_labelD = new JLabel();
+    protected final JLabel seconds_left = new JLabel();
+    protected final JTextField number_right = new JTextField();
+    protected final JTextField percentage = new JTextField();
+    protected final JLabel lives_left = new JLabel();
 
-    private final JFrame frame = new JFrame();
-    private final JTextField textField = new JTextField();
-    private final JTextArea textarea = new JTextArea();
-    private final JButton buttonA = new JButton();
-    private final JButton buttonB = new JButton();
-    private final JButton buttonC = new JButton();
-    private final JButton buttonD = new JButton();
-    private final JButton buttonRepeat = new JButton();
-    private final JButton buttonToMenu = new JButton();
-    private final JLabel answer_labelA = new JLabel();
-    private final JLabel answer_labelB = new JLabel();
-    private final JLabel answer_labelC = new JLabel();
-    private final JLabel answer_labelD = new JLabel();
-    private final JLabel seconds_left = new JLabel();
-    private final JLabel lives_left = new JLabel();
-    private final JTextField number_right = new JTextField();
-    private final JTextField percentage = new JTextField();
 
     private final Timer timer = new Timer(1000, e -> {
         seconds--;
@@ -80,46 +76,57 @@ public class GameFrame implements ActionListener {
         textField.setBounds(0, 0, 650, 50);
         textField.setBackground(new Color(234, 69, 69));
         textField.setForeground(new Color(219, 206, 206));
-        textField.setFont(new Font("Ink Free", Font.BOLD, 30));
+        textField.setFont(new Font("Courier", Font.BOLD, 30));
         textField.setBorder(BorderFactory.createBevelBorder(1));
         textField.setHorizontalAlignment(JTextField.CENTER);
         textField.setEditable(false);
 
-        textarea.setBounds(0, 50, 650, 50);
-        textarea.setLineWrap(true);
-        textarea.setWrapStyleWord(true);
+        textarea.setBounds(10, 50, 600, 75);
+        // textarea.setLineWrap(true);
+        //  textarea.setWrapStyleWord(true);
         textarea.setBackground(new Color(170, 66, 66));
         textarea.setForeground(new Color(219, 206, 206));
-        textarea.setFont(new Font("Courier", Font.BOLD, 25));
+        textarea.setFont(new Font("Courier", Font.BOLD, 20));
         textarea.setBorder(BorderFactory.createBevelBorder(1));
         textarea.setEditable(false);
+        StyledDocument doc = textarea.getStyledDocument();
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+        doc.setParagraphAttributes(10, doc.getLength() - 1, center, false);
 
-        buttonA.setBounds(0, 100, 100, 100);
-        buttonA.setFont(new Font("Courier", Font.BOLD, 35));
+        JButton[] jButtons = {buttonA, buttonB, buttonC, buttonD};
+for (JButton j: jButtons)
+{
+    j.setBackground(new Color(170, 66, 66));
+    j.setForeground(new Color(219, 206, 206));
+
+}
+        buttonA.setBounds(10, 150, 100, 100);
+        buttonA.setFont(new Font("Courier", Font.BOLD, 25));
         buttonA.setFocusable(false);
         buttonA.addActionListener(this);
         buttonA.setText("A");
 
-        buttonB.setBounds(0, 200, 100, 100);
-        buttonB.setFont(new Font("Courier", Font.BOLD, 35));
+        buttonB.setBounds(10, 250, 100, 100);
+        buttonB.setFont(new Font("Courier", Font.BOLD, 25));
         buttonB.setFocusable(false);
         buttonB.addActionListener(this);
         buttonB.setText("B");
 
-        buttonC.setBounds(0, 300, 100, 100);
-        buttonC.setFont(new Font("Courier", Font.BOLD, 35));
+        buttonC.setBounds(10, 350, 100, 100);
+        buttonC.setFont(new Font("Courier", Font.BOLD, 25));
         buttonC.setFocusable(false);
         buttonC.addActionListener(this);
         buttonC.setText("C");
 
-        buttonD.setBounds(0, 400, 100, 100);
-        buttonD.setFont(new Font("Courier", Font.BOLD, 35));
+        buttonD.setBounds(10, 450, 100, 100);
+        buttonD.setFont(new Font("Courier", Font.BOLD, 25));
         buttonD.setFocusable(false);
         buttonD.addActionListener(this);
         buttonD.setText("D");
 
         buttonRepeat.setBounds(225, 490, 200, 50);
-        buttonRepeat.setFont(new Font("Ink Free", Font.BOLD, 25));
+        buttonRepeat.setFont(new Font("Courier", Font.BOLD, 25));
         buttonRepeat.setFocusable(false);
         buttonRepeat.addActionListener(this);
         buttonRepeat.setText("Repeat");
@@ -127,7 +134,7 @@ public class GameFrame implements ActionListener {
         buttonRepeat.setForeground(new Color(219, 206, 206));
 
         buttonToMenu.setBounds(225, 550, 200, 50);
-        buttonToMenu.setFont(new Font("Ink Free", Font.BOLD, 25));
+        buttonToMenu.setFont(new Font("Courier", Font.BOLD, 25));
         buttonToMenu.setFocusable(false);
         buttonToMenu.addActionListener(this);
         buttonToMenu.setText("Back to Menu");
@@ -135,39 +142,41 @@ public class GameFrame implements ActionListener {
         buttonToMenu.setForeground(new Color(219, 206, 206));
 
 
-        answer_labelA.setBounds(125, 100, 500, 100);
+        answer_labelA.setBounds(125, 150, 500, 100);
         answer_labelA.setBackground(new Color(50, 50, 50));
         answer_labelA.setForeground(new Color(219, 206, 206));
-        answer_labelA.setFont(new Font("Courier", Font.PLAIN, 35));
+        answer_labelA.setFont(new Font("Courier", Font.PLAIN, 25));
 
-        answer_labelB.setBounds(125, 200, 500, 100);
+        answer_labelB.setBounds(125, 250, 500, 100);
         answer_labelB.setBackground(new Color(50, 50, 50));
         answer_labelB.setForeground(new Color(219, 206, 206));
-        answer_labelB.setFont(new Font("Courier", Font.PLAIN, 35));
+        answer_labelB.setFont(new Font("Courier", Font.PLAIN, 25));
 
-        answer_labelC.setBounds(125, 300, 500, 100);
+        answer_labelC.setBounds(125, 350, 500, 100);
         answer_labelC.setBackground(new Color(50, 50, 50));
         answer_labelC.setForeground(new Color(219, 206, 206));
-        answer_labelC.setFont(new Font("Courier", Font.PLAIN, 35));
+        answer_labelC.setFont(new Font("Courier", Font.PLAIN, 25));
 
-        answer_labelD.setBounds(125, 400, 500, 100);
+        answer_labelD.setBounds(125, 450, 500, 100);
         answer_labelD.setBackground(new Color(50, 50, 50));
         answer_labelD.setForeground(new Color(219, 206, 206));
-        answer_labelD.setFont(new Font("Courier", Font.PLAIN, 35));
+        answer_labelD.setFont(new Font("Courier", Font.PLAIN, 25));
 
         seconds_left.setBounds(535, 510, 100, 100);
         seconds_left.setBackground(new Color(25, 25, 25));
         seconds_left.setForeground(new Color(255, 0, 0));
-        seconds_left.setFont(new Font("Ink Free", Font.BOLD, 60));
+        seconds_left.setFont(new Font("Courier", Font.BOLD, 60));
         seconds_left.setBorder(BorderFactory.createBevelBorder(1));
         seconds_left.setOpaque(true);
         seconds_left.setHorizontalAlignment(JTextField.CENTER);
         seconds_left.setText(String.valueOf(seconds));
-//
+
+
+        lives_left.setVisible(this instanceof SurvivalGame);
         lives_left.setBounds(535, 140, 100, 100);
         lives_left.setBackground(new Color(25, 25, 25));
         lives_left.setForeground(new Color(255, 0, 0));
-        lives_left.setFont(new Font("Ink Free", Font.BOLD, 60));
+        lives_left.setFont(new Font("Courier", Font.BOLD, 60));
         lives_left.setBorder(BorderFactory.createBevelBorder(1));
         lives_left.setOpaque(true);
         lives_left.setHorizontalAlignment(JTextField.CENTER);
@@ -177,14 +186,14 @@ public class GameFrame implements ActionListener {
         time_label.setBounds(535, 475, 100, 25);
         time_label.setBackground(new Color(50, 50, 50));
         time_label.setForeground(new Color(255, 0, 0));
-        time_label.setFont(new Font("MV Boli", Font.PLAIN, 16));
+        time_label.setFont(new Font("Courier", Font.PLAIN, 16));
         time_label.setHorizontalAlignment(JTextField.CENTER);
         time_label.setText("timer >:D");
 
         number_right.setBounds(225, 225, 200, 100);
         number_right.setBackground(new Color(219, 206, 206));
         number_right.setForeground(new Color(234, 69, 69));
-        number_right.setFont(new Font("Ink Free", Font.BOLD, 50));
+        number_right.setFont(new Font("Courier", Font.BOLD, 50));
         number_right.setBorder(BorderFactory.createBevelBorder(1));
         number_right.setHorizontalAlignment(JTextField.CENTER);
         number_right.setEditable(false);
@@ -192,7 +201,7 @@ public class GameFrame implements ActionListener {
         percentage.setBounds(225, 325, 200, 100);
         percentage.setBackground(new Color(234, 69, 69));
         percentage.setForeground(new Color(219, 206, 206));
-        percentage.setFont(new Font("Ink Free", Font.BOLD, 50));
+        percentage.setFont(new Font("Courier", Font.BOLD, 50));
         percentage.setBorder(BorderFactory.createBevelBorder(1));
         percentage.setHorizontalAlignment(JTextField.CENTER);
         percentage.setEditable(false);
@@ -224,7 +233,7 @@ public class GameFrame implements ActionListener {
 
     private void nextQuestion() {
 
-        if (index >= settings.getTotal_questions()) {
+        if (index >= settings.getTotal_questions() || numberLives < 0) {
             results();
         } else {
             textField.setText("Question " + (index + 1));
@@ -234,54 +243,6 @@ public class GameFrame implements ActionListener {
             answer_labelC.setText(questions[index].getAnswerOptions()[2]);
             answer_labelD.setText(questions[index].getAnswerOptions()[3]);
             timer.start();
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-        buttonA.setEnabled(false);
-        buttonB.setEnabled(false);
-        buttonC.setEnabled(false);
-        buttonD.setEnabled(false);
-
-        if (e.getSource() == buttonA) {
-            answer = "A";
-            if (answer.equals(questions[index].getCorrectAnswer())) {
-                correct_guesses++;
-            }
-            displayAnswer();
-        }
-        if (e.getSource() == buttonB) {
-            answer = "B";
-            if (answer.equals(questions[index].getCorrectAnswer())) {
-                correct_guesses++;
-            }
-            displayAnswer();
-        }
-        if (e.getSource() == buttonC) {
-            answer = "C";
-            if (answer.equals(questions[index].getCorrectAnswer())) {
-                correct_guesses++;
-            }
-            displayAnswer();
-        }
-        if (e.getSource() == buttonD) {
-            answer = "D";
-            if (answer.equals(questions[index].getCorrectAnswer())) {
-                correct_guesses++;
-            }
-            displayAnswer();
-        }
-
-
-        if (e.getSource() == buttonRepeat) {
-            frame.dispose();
-            GameFrame gameFrame = new GameFrame();
-        }
-        if (e.getSource() == buttonToMenu) {
-            frame.dispose();
-            MainMenuFrame mainMenuFrame = new MainMenuFrame();
         }
     }
 
@@ -303,7 +264,7 @@ public class GameFrame implements ActionListener {
         if (!questions[index].getCorrectAnswer().equals("D"))
             answer_labelD.setForeground(new Color(255, 0, 0));
 
-        Timer pause = new Timer(2000, e -> {
+        Timer pause = new Timer(1000, e -> {
 
             answer_labelA.setForeground(new Color(25, 255, 0));
             answer_labelB.setForeground(new Color(25, 255, 0));
@@ -313,6 +274,9 @@ public class GameFrame implements ActionListener {
             answer = " ";
             seconds = settings.getSeconds();
             seconds_left.setText(String.valueOf(seconds));
+
+            lives_left.setText(String.valueOf(numberLives));
+
             buttonA.setEnabled(true);
             buttonB.setEnabled(true);
             buttonC.setEnabled(true);
