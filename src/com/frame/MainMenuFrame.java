87483edeec791aff589c18f20b824a7d9d20309a
Patotski-AS <7906241@gmail.com>
2021-02-07 15:23:55 +0300

    готово

--- src/com/frame/MainMenuFrame.java
+++ src/com/frame/MainMenuFrame.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 public class MainMenuFrame extends JFrame implements ActionListener {
     private final Settings settings = Settings.readSettings();
     private final JFrame frame;
+
     private final JButton buttonA = new JButton();
     private final JButton buttonB = new JButton();
     private final JButton buttonC = new JButton();
@@ -26,8 +27,7 @@ public class MainMenuFrame extends JFrame implements ActionListener {
         frame.setResizable(false);
         frame.setVisible(true);
 
-
-        buttonA.setBounds(200, 100, 300, 100);
+        buttonA.setBounds(200, 100, 300, 90);
         buttonA.setFont(new Font("Courier", Font.BOLD, 35));
         buttonA.setFocusable(false);
         buttonA.addActionListener(this);
@@ -35,7 +35,7 @@ public class MainMenuFrame extends JFrame implements ActionListener {
         buttonA.setForeground(new Color(95, 51, 51));
         buttonA.setText("START GAME");
 
-        buttonB.setBounds(200, 200, 300, 100);
+        buttonB.setBounds(200, 200, 300, 90);
         buttonB.setFont(new Font("Courier", Font.BOLD, 35));
         buttonB.setFocusable(false);
         buttonB.addActionListener(this);
@@ -43,7 +43,7 @@ public class MainMenuFrame extends JFrame implements ActionListener {
         buttonB.setForeground(new Color(95, 51, 51));
         buttonB.setText("RECORDS");
 
-        buttonC.setBounds(200, 300, 300, 100);
+        buttonC.setBounds(200, 300, 300, 90);
         buttonC.setFont(new Font("Courier", Font.BOLD, 35));
         buttonC.setFocusable(false);
         buttonC.addActionListener(this);
@@ -51,7 +51,7 @@ public class MainMenuFrame extends JFrame implements ActionListener {
         buttonC.setForeground(new Color(95, 51, 51));
         buttonC.setText("SETTINGS");
 
-        buttonD.setBounds(200, 400, 300, 100);
+        buttonD.setBounds(200, 400, 300, 90);
         buttonD.setFont(new Font("Courier", Font.BOLD, 35));
         buttonD.setFocusable(false);
         buttonD.addActionListener(this);
@@ -59,11 +59,21 @@ public class MainMenuFrame extends JFrame implements ActionListener {
         buttonD.setForeground(new Color(95, 51, 51));
         buttonD.setText("EXIT");
 
+        JLabel text = new JLabel("QUIZ FOR MASHA");
+        text.setFont(new Font("Courier", Font.BOLD, 35));
+        text.setVisible(true);
+
+        text.setAlignmentX(JTextField.CENTER);
+        text.setBackground(new Color(50, 50, 50));
+        text.setForeground(new Color(219, 206, 206));
+        text.setBounds(200, 25, 350, 50);
+        frame.add(text);
 
         frame.add(buttonA);
         frame.add(buttonB);
         frame.add(buttonC);
         frame.add(buttonD);
+
 
     }
 
@@ -72,24 +82,26 @@ public class MainMenuFrame extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         //режимы игры
         if (e.getSource() == buttonA) {
+            frame.dispose();
             switch (settings.getModeGame()) {
                 case ("Timer"):
-                    new TimerGame();
+                    GameFrame timerGame = new TimerGame();
                     break;
                 case ("Survival"):
-                    new SurvivalGame();
+                    GameFrame survivalGame = new SurvivalGame();
                     break;
             }
+
         }
         if (e.getSource() == buttonB) {
             //ОТКРЫВАЕМ ОКНО РЕКОРДОВ
             frame.dispose();
-            new RecordsMenuFrame();
+            RecordsMenuFrame settingsFrame = new RecordsMenuFrame();
         }
         //настройки
         if (e.getSource() == buttonC) {
             frame.dispose();
-            new SettingsFrame();
+            SettingsFrame settingsFrame = new SettingsFrame();
         }
         //выход
         if (e.getSource() == buttonD) {
